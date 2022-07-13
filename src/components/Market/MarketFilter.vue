7724c04e8e20df17e5cb6429a2db86d97c9eeb8a
shani kupiec <shani.kupiec@gmail.com>
2022-07-13 14:04:15 +0300

    market filter styling

--- src/components/Market/MarketFilter.vue
+++ src/components/Market/MarketFilter.vue
@@ -1,15 +1,21 @@
 <template>
-  <section>
-    <div>
-      <select v-for="topic in filterOptions" :key="topic.name">
-        <option v-for="option in topic.options" :key="option" :value="option">{{ option }}</option>
-      </select>
-      <button>x</button>
-      <button>x</button>
-    </div>
-    <div>
-      <span>{{ from }} -</span>
-      <span>{{ to }}</span>
+  <section class="market-filter">
+    <section class="market-filter__left">
+      <div class="market-filter__left__selects">
+        <select v-for="topic in filterOptions" :key="topic.name">
+          <option v-for="option in topic.options" :key="option" :value="option">{{ option }}</option>
+        </select>
+      </div>
+      <div class="market-filter__left__btns">
+        <div>placeholder</div>
+        <div>placeholder</div>
+      </div>
+    </section>
+    <div class="market-filter__right">
+      <!-- <span>{{ from }} -</span>
+      <span>{{ to }}</span> ADD -->
+      <span>date</span>
+      <img src="../../assets/images/date-icon.png" class="market-filter__right__img" alt="" />
     </div>
   </section>
 </template>
