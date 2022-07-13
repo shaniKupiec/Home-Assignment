76c7c00e18653b78b131d4ef1fd090868a68e929
shani kupiec <shani.kupiec@gmail.com>
2022-07-13 11:58:28 +0300

    project setup

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,22 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <section>
+    html
+  </section>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+// import appHeader from './components/app-header.vue'
 
-nav {
-  padding: 30px;
+export default {
+//  props: {
+//    car: Object,
+//  },
+  components: {},
+  data() {
+    return {}
+  },
+  created() {},
+  methods: {},
+  computed: {},
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
